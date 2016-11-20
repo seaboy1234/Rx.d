19b2888547790d7f10e03aac644857d2cd107b83
Christian Wilson <myself@christianwilson.me>
2016-11-20 15:58:49 -0800

    Implement Replay operator; refactor publishing.
    The replay operator works similar to Publish except that it uses a ReplaySubject.

--- src/reactived/observable/operators/connectable.d
+++ src/reactived/observable/operators/connectable.d
@@ -7,31 +7,39 @@ import reactived.subject;
 import reactived.disposable;
 import disposable = reactived.disposable;
 
+private template publishSubject(TSubject)
+{
+    ConnectableObservable!T publishSubject(T)(Observable!T observable)
+    {
+        static class AnonymousConnectableObservable : ConnectableObservable!T
+        {
+            private TSubject _subject;
+            private Observable!T _observable;
+
+            this(Observable!T observable)
+            {
+                _subject = new TSubject();
+                _observable = observable;
+            }
+
+            void connect()
+            {
+                _observable.subscribe(_subject);
+            }
+
+            Disposable subscribe(Observer!T observer)
+            {
+                return _subject.subscribe(observer);
+            }
+        }
+
+        return new AnonymousConnectableObservable(observable);
+    }
+}
+
 ConnectableObservable!T publish(T)(Observable!T observable)
 {
-    static class AnonymousConnectableObservable : ConnectableObservable!T
-    {
-        private Subject!T _subject;
-        private Observable!T _observable;
-
-        this(Observable!T observable)
-        {
-            _subject = new Subject!T();
-            _observable = observable;
-        }
-
-        void connect()
-        {
-            _observable.subscribe(_subject);
-        }
-
-        Disposable subscribe(Observer!T observer)
-        {
-            return _subject.subscribe(observer);
-        }
-    }
-
-    return new AnonymousConnectableObservable(observable);
+    return publishSubject!(Subject!T)(observable);
 }
 
 unittest
@@ -56,6 +64,39 @@ unittest
     o.dump("publish()");
 
     o.connect();
+
+    currentThreadScheduler.work();
+}
+
+ConnectableObservable!T replay(T)(Observable!T source)
+{
+    return publishSubject!(ReplaySubject!T)(source);
+}
+
+unittest
+{
+    import std.random : Random, unpredictableSeed;
+    import reactived.util : dump;
+    import reactived.scheduler : observeOn, currentThreadScheduler;
+
+    // dfmt off
+    auto o = Random(unpredictableSeed).asObservable()
+                                      .observeOn(currentThreadScheduler)
+                                      .take(10)
+                                      .replay();
+    // dfmt on
+
+    uint[] items = (uint[]).init;
+
+    o.subscribe((x) { items ~= x; });
+
+    o.dump("replay()");
+
+    o.connect();
+
+    currentThreadScheduler.work();
+
+    o.subscribe((x) => assert(items[$ - 1] == x));
 
     currentThreadScheduler.work();
 }
