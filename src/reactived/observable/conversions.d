56c9363175b754759869acadc6fdc859f6737f67
Christian Wilson <myself@christianwilson.me>
2016-11-26 11:18:52 -0800

    Implement an AsTask conversion.
    AsTask represents the *last* value of the source observable as a Task.

--- src/reactived/observable/conversions.d
+++ src/reactived/observable/conversions.d
@@ -1,6 +1,7 @@
 module reactived.observable.conversions;
 
 import std.range.primitives;
+import std.parallelism;
 
 import reactived.disposable : BooleanDisposable, Disposable;
 import reactived.observable.types;
@@ -77,4 +78,64 @@ unittest
     currentThreadScheduler.work();
 
     assert(completed);
+}
+
+auto asTask(T)(Observable!T source)
+{
+    static T getValue(Observable!T source)
+    {
+        bool completed;
+        Throwable exception;
+        T lastValue;
+
+        void onNext(T value)
+        {
+            lastValue = value;
+        }
+
+        void onCompleted()
+        {
+            completed = true;
+        }
+
+        void onError(Throwable error)
+        {
+            exception = error;
+            onCompleted();
+        }
+
+        source.observeOn(currentThreadScheduler).subscribe(&onNext, &onCompleted, &onError);
+
+        while (!completed)
+        {
+            currentThreadScheduler.work();
+        }
+
+        if (exception !is null)
+        {
+            throw exception;
+        }
+
+        return lastValue;
+    }
+
+    return task!getValue(source);
+}
+
+unittest
+{
+    import reactived.observable : map, timer, doOnNext;
+    import std.datetime : dur;
+    import std.stdio : writeln;
+    import std.string : format;
+
+    auto t = timer(dur!"seconds"(1)).map!(v => 100).doOnNext((int v) {
+        writeln("asTask(", v, ")");
+    }).asTask();
+
+    taskPool.put(t);
+
+    auto val = t.yieldForce();
+
+    assert(val == 100, format("Expected 100.  Got %d.", val));
 }
